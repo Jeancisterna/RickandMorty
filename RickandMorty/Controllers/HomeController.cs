210bfa12e3a1b5bbf98ed9d305b2932e961ee6dc
CRS80214 <CRS80214@TJM17020001>
2023-01-20 08:26:42 -0300

    Add project files.

---
+++ RickandMorty/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RickandMorty.Models;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace RickandMorty.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public async Task<IActionResult> Privacy()
+		{
+			string url = "https://rickandmortyapi.com/api/character";
+			var client = new HttpClient();
+			var response = await client.GetAsync(url);
+			var body = await response.Content.ReadAsStringAsync();
+			var rick = JsonSerializer.Deserialize<Root>(body);
+
+
+			return View("Privacy",rick);
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
